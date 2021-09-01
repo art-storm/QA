baff53ef4b8e5042692c1186e748a23de49dc24b
Igor <igor@art-storm.com>
2021-09-02 01:19:51 +0300

    update terraform files

--- terraform/common_resource.tf
+++ terraform/common_resource.tf
@@ -74,6 +74,18 @@ resource "azurerm_network_security_group" "main" {
         destination_address_prefix = "*"
     }
 
+  security_rule {
+        name                       = "PostgreSQL"
+        priority                   = 1030
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "5432"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
     tags = {
         environment = "testing"
     }
---
+++ terraform/wepapp.tf
@@ -0,0 +1,19 @@
+resource "azurerm_app_service_plan" "webapp" {
+  name                = "${var.prefix}-appserviceplan"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "webapp" {
+  name                = "${var.prefix}-app-service"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_app_service_plan.webapp.id
+}
