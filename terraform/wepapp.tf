3e39cae36fe031b6f19899b293fcce412abe4643
Igor <igor@art-storm.com>
2021-09-08 10:25:17 +0300

    update terraform

--- terraform/wepapp.tf
+++ terraform/wepapp.tf
@@ -1,3 +1,4 @@
+
 resource "azurerm_app_service_plan" "webapp" {
   name                = "${var.prefix}-appserviceplan"
   location            = azurerm_resource_group.main.location
@@ -16,4 +17,14 @@ resource "azurerm_app_service" "webapp" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   app_service_plan_id = azurerm_app_service_plan.webapp.id
+
+  site_config {
+    linux_fx_version  = "DOCKER|registrydockerimages/test-app-java:latest"
+  }
+
+  app_settings = {
+    DOCKER_REGISTRY_SERVER_URL            = "https://${azurerm_container_registry.acr.login_server}"
+    DOCKER_REGISTRY_SERVER_USERNAME       = azurerm_container_registry.acr.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD       = azurerm_container_registry.acr.admin_password
+  }
 }
